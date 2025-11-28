b53075abeb75c11868bfe62d5edc608d02988a09
Onofre Souza <souzaonofre@gmail.com>
2025-11-28 16:45:35 -0300

    adic. campos p/ rastreo processamento validacao

--- db/migrate/20251128113531_create_imported_files.rb
+++ db/migrate/20251128113531_create_imported_files.rb
@@ -2,11 +2,15 @@ class CreateImportedFiles < ActiveRecord::Migration[8.0]
   def change
     create_table :imported_files do |t|
       t.string :file_hash
+      
       t.string :email_from, null: true
       t.string :email_to, null: true
       t.string :email_subject, null: true
       t.text :email_body, null: true
+
       t.boolean :is_valid, default: false
+      t.string :validation_source, null: true
+      t.string :validation_error, null: true
 
       t.timestamps
     end
