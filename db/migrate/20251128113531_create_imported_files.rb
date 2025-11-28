1ec39cd9027a9745c736b730475dfca40b369d14
Onofre Souza <souzaonofre@gmail.com>
2025-11-28 15:50:56 -0300

    Correcao opcao 'null' deve ser definida.

--- db/migrate/20251128113531_create_imported_files.rb
+++ db/migrate/20251128113531_create_imported_files.rb
@@ -2,10 +2,10 @@ class CreateImportedFiles < ActiveRecord::Migration[8.0]
   def change
     create_table :imported_files do |t|
       t.string :file_hash
-      t.string :email_from, :null
-      t.string :email_to, :null
-      t.string :email_subject, :null
-      t.text :email_body, :null
+      t.string :email_from, null: true
+      t.string :email_to, null: true
+      t.string :email_subject, null: true
+      t.text :email_body, null: true
       t.boolean :is_valid, default: false
 
       t.timestamps
