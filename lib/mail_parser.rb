4639f4e1c12ac20e143304ea6a27d843e17fba10
Onofre Souza <souzaonofre@gmail.com>
2025-12-01 11:24:31 -0300

    Correcao validacao e atualizacao de teste

--- lib/mail_parser.rb
+++ lib/mail_parser.rb
@@ -10,7 +10,7 @@ module MailParser
       raise ArgumentError.new("Invalid 'file_content' param.")
     end
 
-    raise ArgumentError.new("Exceeded max size limit to 'file_content'.") if file_content > Constants::MAX_FILE_CONTENT_SIZE
+    raise ArgumentError.new("Exceeded max size limit to 'file_content'.") if file_content.size > Constants::MAX_FILE_CONTENT_SIZE
 
     file_type = file_type.nil? ? Constants::DEFAULT_FILE_TYPE : file_type
     raise ArgumentError.new("Invalid 'file_type' param.") unless Constants::MAIL_FILE_TYPES.index(file_type) >= 0
--- spec/lib/mail_parser_spec.rb
+++ spec/lib/mail_parser_spec.rb
@@ -3,26 +3,19 @@ require "rails_helper"
 RSpec.describe "MailParser", type: :class do
   # Global variables
   let(:email_pedido_valido) { File.read(Rails.root.join("spec/fixtures/files/email_pedido_valido.eml")) }
-  let(:blob_email_pedido_valid) {
-    ActiveStorage::Blob.create_and_upload!(
-      io: StringIO.new(email_pedido_valido),
-      filename: "email_valid.eml",
-      content_type: "message/rfc822",
-    )
-  }
   let(:valid_args_instance) {
-    MailParser.builder(storage_blob: blob_email_pedido_valid, file_type: :eml)
+    MailParser.builder(file_content: email_pedido_valido, file_type: :eml)
   }
-  let(:only_storage_blob_arg_instance) {
-    MailParser.builder(storage_blob: blob_email_pedido_valid)
+  let(:only_file_content_arg_instance) {
+    MailParser.builder(file_content: email_pedido_valido)
   }
 
   describe "#builder" do
     it "When has valid args and is instance of MailParser::Eml returns true" do
       expect(valid_args_instance.instance_of?(MailParser::Eml)).to be true
     end
-    it "When has only storage_blob arg. and is instance of MailParser::Eml returns true" do
-      expect(only_storage_blob_arg_instance.instance_of?(MailParser::Eml)).to be true
+    it "When has only file_content arg. and is instance of MailParser::Eml returns true" do
+      expect(only_file_content_arg_instance.instance_of?(MailParser::Eml)).to be true
     end
     it "When is no arguments instance to raise_error" do
       expect { MailParser.builder() }.to raise_error(ArgumentError)
