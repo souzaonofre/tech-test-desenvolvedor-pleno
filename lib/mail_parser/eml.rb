21cfc506e7587aca89420a40a10d1b7f109bd886
Onofre Souza <souzaonofre@gmail.com>
2025-12-01 11:25:43 -0300

    Iniciando impl. parser eml

--- lib/mail_parser/eml.rb
+++ lib/mail_parser/eml.rb
@@ -2,13 +2,30 @@ require "mail"
 
 module MailParser
   class Eml
-    attr_reader :storage
+    attr_reader :email_content, :message_header_fields, :error_message
 
     def initialize(file_content)
       @email_content = file_content
+      @message_header_fields = nil
+      @error_message = nil
     end
 
-    def header_parse
+    def message_parse
+      msg = Mail.new(@email_content)
+      @message_header_fields = msg.header_fields
+    rescue Mail::Field::NilParseError, Mail::Field::ParseError
+      @error_message = "Fail on tray parser 'eml' content type"
+      @message_header_fields = nil
+    end
+
+    def message_header_fields
+      return nil if @message_header_fields.nil? or not @message_header_fields.instance_of?(Mail::FieldList)
+      @message_header_fields
+    end
+
+    def message_from
+      return nil if message_header_fields.nil?
+      @message_header_fields.from
     end
   end
 end
