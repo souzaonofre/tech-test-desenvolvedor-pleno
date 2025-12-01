57f232f750447afec452da0a681ef70a74fc3573
Onofre Souza <souzaonofre@gmail.com>
2025-12-01 11:27:13 -0300

    Reutilzando vars. globais p/ testes

--- spec/services/file/mail_import.rb
+++ spec/services/file/mail_import.rb
@@ -1,4 +1,12 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe MailImport, type: :class do
+  let(:email_pedido_valido) { File.read(Rails.root.join("spec/fixtures/files/email_pedido_valido.eml")) }
+  let(:blob_email_pedido_valid) {
+    ActiveStorage::Blob.create_and_upload!(
+      io: StringIO.new(email_pedido_valido),
+      filename: "email_valid.eml",
+      content_type: "message/rfc822",
+    )
+  }
 end
