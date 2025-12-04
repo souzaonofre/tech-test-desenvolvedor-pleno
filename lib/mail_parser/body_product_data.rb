2df9282a7963e085f7ccad038bc9cd8e5e1ddb8d
Onofre Souza <souzaonofre@gmail.com>
2025-12-04 08:19:52 -0300

    Impl. parser de produto no campo assunto

--- lib/mail_parser/body_product_data.rb
+++ lib/mail_parser/body_product_data.rb
@@ -2,7 +2,7 @@ require "mail"
 
 module MailParser
   class BodyProductData
-    attr_reader :body_stream, :subject_line, :product_data, :product_unparsed_lines, :error_message
+    attr_reader :body_stream, :subject_product, :body_product, :product_unparsed_lines, :error_message
 
     BODY_PRODUCT_KEY_DICT = [
       "produto:",
@@ -15,42 +15,66 @@ module MailParser
     SUBJECT_DATA_KEY = "subject:"
 
     SUBJECT_PRODUCT_KEY_DICT = [
-      "- produto ",
-      "- prod-"
+      "- Produto ",
+      "- PROD-"
     ]
 
     def initialize(body_with_subject_content)
       @body_stream = body_with_subject_content
       @product_unparsed_lines = []
-      @product_data = nil
+      @body_product = nil
       @error_message = nil
-      scan_body_stream
+      @subject_product = nil
+      scan_subject_product!
     end
 
-    def get_subject_data
-    end
-
-    def scan_body_stream
-      StringIO.new(@body_stream).each_line do |line|
-        # line = strio.gets
-        line.chop!.strip!
-        unless line.blank?
-          unless line.blank? or line.index(":").nil? or (line.index(":") < 3)
-            @body_unparsed_lines << line
-            parts = line.split(":")
-            key = parts[0]
-            unless parts.blank? or key.blank? or (parts.size > 2)
-              key.downcase!
-              key.tr!(" ", "_").tr!("-", "_") unless Mail::Utilities.atom_safe?(key)
-              @body_fields << { "#{key}": parts[1].strip! }
-            end
-          end
+    def parse_subject_product(subject_data)
+      return if subject_data.nil?
+      subject_data = String(subject_data)
+      SUBJECT_PRODUCT_KEY_DICT.each do |pattern|
+        pattern = String(pattern)
+        # debugger
+        pos = subject_data.index(pattern)
+        unless pos.nil?
+          pos += pattern.size
+          @subject_product = subject_data.slice(pos, subject_data.size) unless pos.nil?
         end
       end
-    rescue IOError, RuntimeError
-      @error_message = "Fail on tray parser 'eml' content type"
-      @body_stream = nil
-      @body_fields = []
     end
+
+    def scan_subject_product!
+      StringIO.new(@body_stream).each_line do |line|
+        line.chop!.strip!
+        rcd = Utils.basic_record_parser(line, ":")
+        unless rcd.nil? or (rcd.keys.size == 0) or (rcd.values.size == 0) or (String(rcd.keys[0]) != "subject")
+          @product_unparsed_lines << line
+          subject_data = String(rcd.values[0]).strip
+          parse_subject_product(subject_data)
+        end
+      end
+    end
+
+    # def scan_body_stream
+    #   StringIO.new(@body_stream).each_line do |line|
+    #     line.chop!.strip!
+    #     unless line.blank? or line.index(":").nil? or (line.index(":") < 3)
+    #       @body_unparsed_lines << line
+    #       parts = line.split(":")
+    #       unless parts.blank? or parts[0].blank? or (parts.size > 2)
+    #         key = String(parts[0]).downcase
+    #         key.tr!(" ", "_").tr!("-", "_") unless Mail::Utilities.atom_safe?(key)
+    #         @body_fields << { "#{key}": String(parts[1]).strip }
+    #       end
+    #     end
+    #   end
+    # rescue IOError, RuntimeError
+    #   @error_message = "Fail on tray parser 'eml' content type"
+    #   @body_stream = nil
+    #   @body_fields = []
+    # end
+  end
+
+  def to_s
+    "product_unparsed_lines: #{@product_unparsed_lines}\n subject_product: #{@subject_product}"
   end
 end
